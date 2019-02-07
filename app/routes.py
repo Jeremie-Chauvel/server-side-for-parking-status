3a3e83ac29fad780f2fb2e4136b5bb4aff674c9f
Jérémie Chauvel <jeremie.chauvel@supelec.fr>
2019-02-07 13:58:41 +0100

    js auto update

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from flask import render_template
-from flask import jsonify
-import request
+import json
+import requests
 from app import app
 
+url = "localhost:5000/parking_update"
 
 parking = [
         {
@@ -21,9 +22,15 @@ def index():
     
     return render_template('index.html', parking=parking)
 
-@app.route('/update/<int:id>/<int:place>', methods=['GET', 'POST'])
+@app.route('/update/<int:id_>/<int:place>', methods=['GET', 'POST'])
 def update(id_, place):
-    """requetes pour mettre à jour le nombre de palce dans une allé"""
+    """requete pour mettre à jour le nombre de palce dans une allé"""
     if id_ in (1,2) and place in (0,1,2,3):
         parking[id_-1]["place"] = place
+    return json.dumps(parking)
+
+@app.route('/get_update', methods=['GET'])
+def get_update():
+    """requete pour recuperer l'état des allés"""
+    return json.dumps(parking)
 
